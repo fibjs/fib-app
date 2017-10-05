5ccc3287fa01b3c23336cf5e45874de74da6457e
xicilion <lion@9465.net>
2017-10-05 17:10:19 +0800

    add session

--- demo/app.js
+++ demo/app.js
@@ -1,11 +1,21 @@
 const http = require('http');
+const util = require('util')
+const Session = require('fib-session')
+
+var session = new Session(new util.LruCache(20000), {
+    timeout: 60 * 1000
+});
+
 const App = require('../');
 
 var app = App('sqlite:test.db', {});
 app.def(require('./defs/pet'));
 app.def(require('./defs/person'));
 
-var svr = new http.Server(8080, {
-    '/1.0': app.handler
-});
+var svr = new http.Server(8080, [
+    session.cookie_filter,
+    {
+        '/1.0': app.handler
+    }
+]);
 svr.run(() => {});
