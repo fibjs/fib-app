688f4848347d0331c1228e004d4b6cba235bf35f
xicilion <lion@9465.net>
2017-10-20 00:04:25 +0800

    graphql support hasMany.

--- test/graphql.js
+++ test/graphql.js
@@ -27,6 +27,7 @@ describe("graphql", () => {
             }
         });
     });
+
     it('hasOne', () => {
         var rep = http.post(`http://127.0.0.1:8080/1.0/app`, {
             headers: {
@@ -67,4 +68,43 @@ describe("graphql", () => {
         });
     });
 
+    it('hasMany', () => {
+        var rep = http.post(`http://127.0.0.1:8080/1.0/app`, {
+            headers: {
+                'Content-Type': 'application/graphql'
+            },
+            body: `{
+                people(id:1){
+                    id,
+                    name,
+                    childs{
+                        id,
+                        name,
+                        mother{
+                            id,
+                            name
+                        }
+                    }
+                }
+            }`
+        });
+
+        assert.equal(rep.statusCode, 200);
+        assert.deepEqual(rep.json(), {
+            "data": {
+                "people": {
+                    "id": "1",
+                    "name": "tom",
+                    "childs": [{
+                        "id": "4",
+                        "name": "lily",
+                        "mother": {
+                            "id": "2",
+                            "name": "alice"
+                        }
+                    }]
+                }
+            }
+        });
+    });
 });
