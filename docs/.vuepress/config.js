c88f29b6c39cf0d77df67e52bf59408d5cf2abfb
richardo2016 <richardo2016@gmail.com>
2019-07-27 03:13:10 +0800

    add vuepress based doc src

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,36 @@
+// @reference https://v1.vuepress.vuejs.org/zh/config/
+
+// .vuepress/config.js
+module.exports = {
+    extend: '@vuepress/theme-default',
+    base: '/fib-app/',
+    title: 'fib-app',
+    description: 'fibjs-based application resources(RESTful API, SSR, Other resources) management framework',
+    head: [
+
+    ],
+    port: 8080,
+    themeConfig: {
+        displayAllHeaders: true,
+        nav: [
+            { text: '首页', link: '/' },
+            { text: '指南', link: '/zh/guide' },
+            { text: 'GITHUB', link: 'http://github.com/fibjs/fib-app' },
+            { text: 'FIBJS', link: 'http://fibjs.org' },
+        //   {
+        //     text: 'Languages',
+        //     items: [
+        //       { text: 'Group1', items: [/*  */] },
+        //       { text: 'Group2', items: [/*  */] }
+        //     ]
+        //   }
+        ],
+        sidebar: [
+            // ['/', '首页'],
+            ['/zh/guide', '指南'],
+            '/zh/app-acl',
+            ['/zh/app-model-extends', 'ORM 扩展选项'],
+            ['/zh/app-internal-api', '内部 API'],
+        ]
+    }
+}
---
+++ docs/index.md
@@ -0,0 +1,16 @@
+---
+home: true
+# heroImage: /hero.png
+heroText: fib-app
+tagline: 为个人网站和中小企业而生
+actionText: 快速上手 →
+actionLink: /zh/guide
+features:
+- title: RESTful
+  details: 内部基于 RESTful 标准实现，一键部署 http 服务
+- title: ORM
+  details: 基于 @fxjs/orm, 快速连接已有关系型数据库（MySQL/SQLite）
+- title: 高性能
+  details: 享受 fibjs 的高性能
+footer: MIT Licensed | Copyright © 2015-present fibjs
+---
