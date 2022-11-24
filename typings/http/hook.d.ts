51312d47428c13831444feba8112ac4a4835dcfb
richardo2016 <richardo2016@gmail.com>
2022-11-24 19:08:42 +0800

    chore: upgrade orm version.

--- typings/http/hook.d.ts
+++ typings/http/hook.d.ts
@@ -1,2 +1,2 @@
-export declare const wait: import("@fxjs/orm/typings/Typo/hook").FxOrmHook.HookWait<import("@fxjs/orm/typings/Typo/instance").FxOrmInstance.Instance, any>;
-export declare const trigger: import("@fxjs/orm/typings/Typo/hook").FxOrmHook.HookTrigger<any, any>;
+export declare const wait: import("@fxjs/orm").FxOrmHook.HookWait<import("@fxjs/orm").FxOrmInstance.Instance, any>;
+export declare const trigger: import("@fxjs/orm").FxOrmHook.HookTrigger<any, any>;
